eb0cb4b02beab6064d4969f2acbead9acf684f8c
Lingzi <susanzhang0521@gmail.com>
2022-11-21 15:51:37 -0500

    added authentication middleware

--- server/middlewares/isAuthenticated.js
+++ server/middlewares/isAuthenticated.js
@@ -1,7 +1,7 @@
 //checks if a user attemped to do some kind of request is logged in
 const isAuthenticated = (req, res, next) => {
     const user = req.session.username
-    if (user === '' || user === undefined) {
+    if (!user) {
       next(new Error('invalid session'))
     } else {
       next()
