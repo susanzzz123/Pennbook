858c2f757ff2a8d2a9fdbfd3b9e1296d97edff4b
Jason Ren <jasoncren22@gmail.com>
2022-12-12 21:59:12 -0500

    commit

--- server/models/database.js
+++ server/models/database.js
@@ -41,6 +41,37 @@ var getUserInfo = function (username, callback) {
   })
 }
 
+// var searchNews = function (string, callback) {
+//   // With username as key
+//   var params = {
+//     KeyConditions: {
+//       username: {
+//         ComparisonOperator: "EQ",
+//         AttributeValueList: [{ S: username }],
+//       },
+//     },
+//     TableName: "users",
+//   }
+
+//   // If the user exists then return the password to the callback
+//   db.query(params, function (err, data) {
+//     if (err || data.Items.length == 0) {
+//       callback(err, "user not found")
+//     } else {
+//       const first_name = data.Items[0].first_name.S
+//       const last_name = data.Items[0].last_name.S
+//       const interests = data.Items[0].interests.SS
+//       const affiliation = data.Items[0].affiliation.S
+//       const username = data.Items[0].username.S
+//       const email = data.Items[0].email.S
+//       const birthday = data.Items[0].birthday.S
+//       const last_time = data.Items[0].last_time.N
+
+//       callback(err, { first_name, last_name, interests, affiliation, username, email, birthday, last_time })
+//     }
+//   })
+// }
+
 var checkLogin = function (username, callback) {
   // With username as key
   var params = {
